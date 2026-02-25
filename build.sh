bbe90edaf66745e17bb754a7baa32f5636ebaa64
soyongkim <smalldragon024@gmail.com>
2026-02-26 00:00:23 +0100

    update deploy functions

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 set -e
 set -o pipefail
 
+export CC=$(which gcc)
+export CXX=$(which g++)
+
 echo "Building Quiche..."
-bazel build //quiche:quic_server //quiche:quic_client //quiche:masque_server //quiche:masque_client || { echo "Bazel build failed"; exit 1; }
+bazel build --repo_env=CC=$CC --repo_env=CXX=$CXX --repo_env=PATH=$PATH //quiche:quic_server //quiche:quic_client //quiche:masque_server //quiche:masque_client || { echo "Bazel build failed"; exit 1; }
 
 echo "Build completed successfully!"
